daf84a4de8d9048cf466604831ee216896c56866
guillaume-chevalier <guillaume-chevalier@outlook.com>
2018-05-09 17:52:04 +0000

    Fix GPU Memory

--- train.py
+++ train.py
@@ -149,31 +149,28 @@ def train(hyperparameters, dataset, evaluation_metric, device):
 
         # Validation
         model.eval()
+        optimizer.zero_grad()
         nb_test_examples = dataset.X_test.shape[0]
-        all_targets = []
         all_outputs = []
         all_losses = []
-        for step, (start, end) in enumerate(
+        for test_step, (start, end) in enumerate(
                 zip(range(0, nb_test_examples, hyperparameters['batch_size']),
                     range(hyperparameters['batch_size'], nb_examples + 1, hyperparameters['batch_size']))):
             inputs = Variable(torch.from_numpy(dataset.X_test[start:end]).float().transpose(1, 0)).to(device)
             targets = Variable(torch.from_numpy(dataset.Y_test[start:end]).long()).to(device)
             outputs, _ = model(inputs, state=None)
             loss = criterion(outputs, targets)
-            optimizer.zero_grad()
-            all_targets.append(targets.to("cpu"))
             all_outputs.append(outputs.to("cpu"))
             all_losses.append(loss.to("cpu"))
-        all_targets = torch.cat(all_targets, dim=0)
         all_outputs = torch.cat(all_outputs, dim=0)
-        all_losses = all_losses.mean()
+        all_losses = np.mean([l.data.item() for l in all_losses])
 
         # Validation metrics
         validation_accuracies.append(metrics.accuracy_score(
-            dataset.Y_test, outputs.argmax(-1)))
+            dataset.Y_test, all_outputs.argmax(-1)))
         validation_f1_scores.append(metrics.f1_score(
-            dataset.Y_test, outputs.argmax(-1), average="weighted"))
-        validation_losses.append(loss.data.item())
+            dataset.Y_test, all_outputs.argmax(-1), average="weighted"))
+        validation_losses.append(all_losses)
 
         # Print
         print("        Validation: accuracy={}, f1={}, loss={}".format(
